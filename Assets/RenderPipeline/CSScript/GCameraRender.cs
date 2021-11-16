d4d6a615769fab122c4481190776a84702df51f3
Shadow <luxiaodong@me.com>
2021-11-16 17:58:39 +0800

    支持渲染不受光的非透明物体

--- Assets/RenderPipeline/CSScript/GCameraRender.cs
+++ Assets/RenderPipeline/CSScript/GCameraRender.cs
@@ -9,6 +9,8 @@ public class GCameraRender
     ScriptableRenderContext m_context;
     const string m_bufferName = "Render Camera";
     CommandBuffer m_buffer = new CommandBuffer{name = m_bufferName};
+    CullingResults m_cullingResult;
+    static ShaderTagId m_unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
 
     public void Init(ScriptableRenderContext context, Camera camera)
     {
@@ -18,23 +20,45 @@ public class GCameraRender
 
     public void Render()
     {
+        if( Cull() == false ) return ;
+
         m_context.SetupCameraProperties(m_camera);
-        m_buffer.ClearRenderTarget(true, true, Color.red);
+        m_buffer.ClearRenderTarget(true, true, Color.clear);
         m_buffer.BeginSample(m_bufferName);
         this.ExecuteBuffer();
 
+        this.DrawOpaqueObject();
         m_context.DrawSkybox(m_camera);
-        
+
         m_buffer.EndSample(m_bufferName);
         this.ExecuteBuffer();
 
         m_context.Submit();
     }
 
-    public void ExecuteBuffer()
+    private void ExecuteBuffer()
     {
         m_context.ExecuteCommandBuffer(m_buffer);
         m_buffer.Clear();
+    }
+
+    private bool Cull()
+    {
+        if(m_camera.TryGetCullingParameters(out ScriptableCullingParameters cullingParameters) )
+        {
+            m_cullingResult = m_context.Cull(ref cullingParameters);
+            return true;
+        }
+
+        return false;
+    }
+
+    private void DrawOpaqueObject()
+    {
+        var sortingSetting = new SortingSettings(m_camera){criteria = SortingCriteria.CommonOpaque};
+        var drawingSetting = new DrawingSettings(m_unlitShaderTagId, sortingSetting);
+        var filteringSetting = new FilteringSettings(RenderQueueRange.opaque);
+        m_context.DrawRenderers(m_cullingResult, ref drawingSetting, ref filteringSetting);
     }
 
 }
